c2fde725d6eeaf689d00ebf9206282f8e36a0962
samSterns <sam.g.sterns@gmail.com>
2019-12-19 13:50:56 -0800

    plugging along

---
+++ __tests__/user.test.js
@@ -0,0 +1,76 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const User = require('../lib/models/User');
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can sign up a user', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ email: 'test@test.com', password: 'password' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          email: 'test@test.com'
+          __v: 0
+        });
+      });
+  });
+
+  it('can log in a user', async() => {
+    const user = await User.create({ email: 'test@test.com', password: 'password' });
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({ email: 'test@test.com', password: 'password' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: user.id,
+          email: 'test@test.com',
+          __v: 0
+        });
+      });
+  });
+
+
+  it('fails when a bad email is used', async () => {
+    await User.create({ email: 'test@test.com', password: 'password' });
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({ email: 'no@test.com', password: 'password' })
+      .then(res => {
+        expect(res.body).toEqual({
+          message: 'Invalid Email/Password',
+          status: 401
+        });
+      });
+  });
+
+  it('fails when a bad password is used', async () => {
+    await User.create({ email: 'test@test.com', password: 'password' });
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({ email: 'test@test.com', password: 'pass' })
+      .then(res => {
+        expect(res.body).toEqual({
+          message: 'Invalid Email/Password',
+          status: 401
+        });
+      });
+  });
+  
+});
